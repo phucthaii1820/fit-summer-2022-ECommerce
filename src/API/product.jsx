8d059aeb86af1aeffd4e5f2a0a2ddbdc772ce269
phucthaii1820 <phucthai0108@gmail.com>
2022-07-23 22:19:50 +0700

    automation search

--- src/API/product.jsx
+++ src/API/product.jsx
@@ -1,7 +1,16 @@
 import callAPI from "./callAPI";
 
 export const getListProducts = (category, page) =>
-    callAPI({ url: `/products/get-list-by-category?category=${category}&page=${page}`, method: 'GET' });
+  callAPI({
+    url: `/products/get-list-by-category?category=${category}&page=${page}`,
+    method: "GET",
+  });
 
-export const getProductInfo = ( id ) =>
-    callAPI({ url: `/products/get-product?id=${id}`, method: 'GET' });
+export const getProductInfo = (id) =>
+  callAPI({ url: `/products/get-product?id=${id}`, method: "GET" });
+
+export const searchProducts = (keyword, page) =>
+  callAPI({
+    url: `/products/search?keyword=${keyword}&page=${page}`,
+    method: "GET",
+  });
