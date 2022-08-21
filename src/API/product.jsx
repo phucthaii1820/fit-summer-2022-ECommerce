d5fb4fe9bbb387cd4d9aebffb98567adb80f5827
phucthaii1820 <phucthai0108@gmail.com>
2022-08-21 10:38:52 +0700

    upload image

--- src/API/product.jsx
+++ src/API/product.jsx
@@ -1,38 +1,43 @@
 import callAPI from "./callAPI";
 
 export const getListProducts = (category, page) =>
-    callAPI({
-        url: `/products/get-list-by-category?category=${category}&page=${page}`,
-        method: "GET",
-    });
+  callAPI({
+    url: `/products/get-list-by-category?category=${category}&page=${page}`,
+    method: "GET",
+  });
 
 export const getProductInfo = (id) =>
-    callAPI({ url: `/products/get-product?id=${id}`, method: "GET" });
+  callAPI({ url: `/products/get-product?id=${id}`, method: "GET" });
 
 export const searchProducts = (keyword, page) =>
-    callAPI({
-        url: `/products/search?keyword=${keyword}&page=${page}`,
-        method: "GET",
-    });
+  callAPI({
+    url: `/products/search?keyword=${keyword}&page=${page}`,
+    method: "GET",
+  });
 
 export const getAllProducts = () =>
-    callAPI({ url: `/products/get-all-products`, method: "GET" });
+  callAPI({ url: `/products/get-all-products`, method: "GET" });
 
-    export const addProduct = (data) =>
-    callAPI({ url: `/products/add-product`, method: "POST", data });
+export const addProduct = (data) =>
+  callAPI({ url: `/products/add-product`, method: "POST", data });
 
 export const updateProduct = (data) =>
-    callAPI({ url: `/products/update-product`, method: "POST", data });
+  callAPI({
+    url: `/products/update-product`,
+    method: "POST",
+    data,
+    option: { headers: { "Content-Type": "multipart/form-data" } },
+  });
 
 export const deleteProduct = (id) =>
-    callAPI({
-        url: `/products/remove-product`,
-        method: "POST",
-        data: { id: id },
-    });
-    
+  callAPI({
+    url: `/products/remove-product`,
+    method: "POST",
+    data: { id: id },
+  });
+
 export const postComment = (data) =>
-    callAPI({ url: `/products/create-comment`, method: "POST", data });
+  callAPI({ url: `/products/create-comment`, method: "POST", data });
 
 export const replyComment = (data) =>
-    callAPI({ url: `/products/reply-comment`, method: "POST", data });
+  callAPI({ url: `/products/reply-comment`, method: "POST", data });
